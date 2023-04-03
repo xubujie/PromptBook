3de23d3271f19bb2ab6c792f7d00a9dd7eb38602
Bujie Xu <bujie.x.xu@gsk.com>
2023-04-03 15:13:41 +0900

    🎨 style(Layout.tsx): add Head component to Layout and set title and favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,15 @@
 import Header from './Header'
 import Footer from '@/components/Footer'
 import type { ReactNode } from 'react'
+import Head from 'next/head'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
+      <Head>
+        <title>PromptBook</title>
+        <link rel='icon' href='/logo.png' />
+      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
